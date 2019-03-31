81b513c5654b7457df31363735f3684a38743c86
saequus <slavaspetsyian@gmail.com>
2019-03-31 14:19:15 +0300

    update 9

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -4,28 +4,41 @@
   <nav-bar></nav-bar>
 
   <div class="container-fluid">
-    <div class="page-container">
-    <div class="row">
+      <div class="page-container">
+        <div class="row">
 
-      <div class="col-2 ml-3">
-        <side-menu></side-menu>
-      </div>
-
-        <div class="col-9 main-window-element box-shadow-line">
-          <div class="container-fluid"><br />
-            <h4>Система мониторинга лесного массива Российской Федерации</h4>
-              <div class="row-8">
-                <span class="text-muted">Доступны обновления с датчиков: 24 </span>
-                <span class="text-muted">Последний запуск мониторинга: 1 час назад</span>
-              </div><br />
+          <div class="col-2 ml-3">
+            <side-menu></side-menu>
           </div>
 
-          <russian-map></russian-map>
+            <div class="col-9 main-window-element box-shadow-line">
+              <div class="container-fluid"><br />
+                <h4>Система мониторинга лесного массива Российской Федерации</h4>
+                  <div class="row-8">
+                    <span class="text-muted">Доступны обновления с датчиков: 24 </span>
+                    <span class="text-muted">Последний запуск мониторинга: 1 час назад</span>
+                  </div><br />
+              </div>
+
+              <div id="layer1">
+                <russian-map></russian-map>
+              </div>
+              <div id="layer2">
+                <svg height="150%" width="100%" class="blinking">
+                  <circle cx="63%" cy="44%" r="5" fill="red"></circle>
+                  <circle cx="30%" cy="40%" r="5" fill="red"></circle>
+                  <circle cx="23%" cy="15%" r="5" fill="red"></circle>
+                  <circle cx="24%" cy="17%" r="5" fill="red"></circle>
+                  <circle cx="52%" cy="79%" r="5" fill="red"></circle>
+                  <circle cx="19%" cy="90%" r="5" fill="red"></circle>
+                  <circle cx="60%" cy="47%" r="5" fill="red"></circle>
+                </svg>
+              </div>
 
 
-        </div>
+            </div>
+          </div>
       </div>
-    </div>
     <div class="row">
       <f></f>
     </div>
@@ -43,18 +56,25 @@
 
   export default {
     name: "ControlPanel",
-      props: ['sensor_id'],
-      data () {
-        return {
-          sensor_id: {},
-        }
-      },
     components: {
       'test': Test,
       'side-menu': SideMenu,
       'russian-map': RussianMap,
       'nav-bar': NavBar,
       'f': F
+    },
+    data() {
+            return {
+              sensors_data: null
+            };
+          },
+    mounted() {
+      axios
+        .get('http://localhost:8000/api/rest/region/0/')
+        .then(response => (this.sensors_data = response.data))
+    },
+    methods: {
+
     }
   }
 </script>
@@ -91,14 +111,64 @@
     border-radius: 0.3rem;
   }
   #layer1, #layer2 {
-    position: relative;
-   }
+  position: relative;
+ }
 
-  #layer2 {
-    top: 50%;
-   }
-   #layer1 { z-index: 2; }
-   #layer2 { z-index: 1; }
+#layer2 {
+  top: -30%;
+ }
+
+ #layer1 { z-index: 1; }
+ #layer2 { z-index: 2; }
+
+  .blinking {
+    -webkit-animation: 1.5s blink ease infinite;
+    -moz-animation: 1.5s blink ease infinite;
+    -ms-animation: 1.5s blink ease infinite;
+    -o-animation: 1.5s blink ease infinite;
+    animation: 1.5s blink ease infinite;
+  }
+
+  @keyframes "blink" {
+    from, to {
+      opacity: 0;
+    }
+      50% {
+        opacity: 1;
+      }
+    }
+    @-moz-keyframes blink {
+      from, to {
+        opacity: 0;
+      }
+      50% {
+        opacity: 1;
+      }
+    }
+    @-webkit-keyframes "blink" {
+      from, to {
+        opacity: 0;
+      }
+      50% {
+        opacity: 1;
+      }
+    }
+    @-ms-keyframes "blink" {
+      from, to {
+        opacity: 0;
+      }
+      50% {
+        opacity: 1;
+      }
+    }
+    @-o-keyframes "blink" {
+      from, to {
+        opacity: 0;
+      }
+      50% {
+        opacity: 1;
+      }
+    }
 
 
 
