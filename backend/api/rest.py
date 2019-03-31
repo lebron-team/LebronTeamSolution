29f6e8e730efb306a2d86a47547e21be069722fa
SmirnoffRD <38382018+SmirnoffRD@users.noreply.github.com>
2019-03-31 10:32:51 +0300

    Запросы по регионам

--- backend/api/rest.py
+++ backend/api/rest.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseNotFound, HttpResponse, HttpResponseForbidden, JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView, View
-from backend.api.models import Area_Point, Sensor, Sensor_Group, Region, Sensor_Data_Set, SensorSerializer, Area_Point_Serializer, GroupSerializer
+from backend.api.models import Area_Point, Sensor, Sensor_Group, Region, Sensor_Data_Set, SensorSerializer, Area_Point_Serializer, GroupSerializer, RegionSerializer
 from django.shortcuts import get_object_or_404
 
 class Get_Sensor(APIView):
@@ -17,8 +17,12 @@ class Get_Sensor(APIView):
 class Get_Data_Set_By_Region(APIView):
     def get(self, requset, id):
         if id == 0:
-            pass
-        pass
+            regions = Region.objects.all()
+            data_region = RegionSerializer(regions, many=True)
+            return Response({'regions': data_region.data})
+        region = Region.objects.get(id=id)
+        data_region = RegionSerializer(region)
+        return Response({'region': data_region.data})
 
 
 class Get_Data_Set_By_Sensor_Group(APIView):
